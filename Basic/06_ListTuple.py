80c2ceb1780424b4ffce1d379d03b252bec48e98
Sergio Madrigal <sergiomadrigal@gmail.com>
2022-09-26 12:28:19 +0200

    06 List Tuple

---
+++ Basic/06_ListTuple.py
@@ -0,0 +1,11 @@
+# 6. Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers
+import sys
+
+numList = sys.argv[1]
+print(type(numList))
+
+arrayList = numList.split(",")
+tupleList = tuple(arrayList)
+
+print("Array:", arrayList)
+print("Tuple:", tupleList)
